fe1d1a41623caec3f81989c6ad1aad6ede8adc5c
haritshammam <siegkounat@gmail.com>
2021-07-15 18:54:14 +0800

    turning the album element into Card component that also has CardInfo component inside that contains Track Title, Artists, and Album info

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from './components/cards'
 import data from './single-sample'
 
 function App() {
@@ -17,32 +18,16 @@ function App() {
       }
     ]
   } = data.album
+  const trackDetails = {trackTitle, albumName, artistName}
 
   return (
-    <div className="flex justify-center mt-2 ">
-      <div className="bg-white-100 w-1/4 px-4 py-4 rounded-lg shadow-lg">
-        <img src={albumImageUrl} alt="Album" className="w-full rounded-lg"/>
-
-        <div className="mt-4">
-          <p className="text-xs font-semibold text-gray-500">Track Title</p>
-          <p className="font-semibold text-gray-900">{trackTitle}</p>
-        </div>
-
-        <div className="mt-3">
-          <p className="text-xs font-semibold text-gray-500">Artist</p>
-          <p className="font-semibold text-gray-900">{artistName}</p>
-        </div>
-
-        <div className="mt-3">
-          <p className="text-xs font-semibold text-gray-500">Album</p>
-          <p className="font-semibold text-gray-900">{albumName}</p>
-        </div>
-
-        <button className="mt-4 bg-gray-800 text-white py-2 w-full text-sm font-semibold rounded-lg hover:bg-gray-900">
-          Select
-        </button>
-      </div>
-    </div>
+    <>
+      <Card 
+        trackDetails={trackDetails}
+        albumImageUrl={albumImageUrl}
+        buttonText="Select"
+      />
+    </>
   )
 }
 
