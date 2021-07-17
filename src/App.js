9744e70f8556cbca5f496da63f70dcaa3a8ea71c
haritshammam <siegkounat@gmail.com>
2021-07-17 15:55:23 +0800

    first try before seminar XLFL. Continue later

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
 import React from 'react'
 import Card from './components/cards'
-import data from './single-sample'
+// import data from './single-sample'
+import datas from './datas/tracksData' 
 
 function App() {
-  // Single's Data
-  const {name: trackTitle} = data
-  const {
-    name: albumName,
-    artists: [
-      {
-        name: artistName
-      }
-    ],
-    images: [
-      {
-        url: albumImageUrl
-      }
-    ]
-  } = data.album
-  const trackDetails = {trackTitle, albumName, artistName}
+  const valueAssignment = (dataParam) => {
+    let {name: trackTitle} = dataParam
+    let {
+      name: albumName,
+      artists: [
+        {
+          name: artistName
+        }
+      ],
+      images: [
+        ,
+        {
+          url: albumImageUrl
+        }
+      ]
+    } = dataParam.album
+    let trackDetails = {trackTitle, albumName, artistName}
+
+    return {
+      albumImageUrl,
+      trackDetails
+    }
+  }
+
+  const renderOneCard = () => {
+    datas.forEach((data, index) => {
+      let { albumImageUrl, trackDetails } = valueAssignment(data)
+      console.log(`${index+1}.`)
+      console.log(albumImageUrl)
+      console.log(trackDetails.trackTitle)
+      console.log(trackDetails.albumName)
+      console.log(trackDetails.artistName)
+      return (
+        <Card
+          trackDetails={trackDetails}
+          albumImageUrl={albumImageUrl}
+          buttonText="Select"
+        />
+      )
+    })
+  }
 
   return (
-    <>
-      <Card 
-        trackDetails={trackDetails}
-        albumImageUrl={albumImageUrl}
-        buttonText="Select"
-      />
-    </>
+    <div className="grid grid-cols-3 place-items-center gap-10 px-40">
+      {renderOneCard()}
+    </div>
   )
 }
 
--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -3,8 +3,7 @@ import CardInfo from './card-info'
 
 const Card = ({trackDetails, albumImageUrl, buttonText}) => {
     return (
-        <div className="flex justify-center mt-2 ">
-            <div className="bg-white-100 w-1/4 px-4 py-4 rounded-lg shadow-lg">
+            <div className="bg-white-100 px-4 py-4 rounded-lg shadow-lg flex flex-col max-w-xs">
                 <img src={albumImageUrl} alt={trackDetails.albumName} className="w-full rounded-lg mb-4"/>
 
                 <CardInfo 
@@ -26,7 +25,6 @@ const Card = ({trackDetails, albumImageUrl, buttonText}) => {
                     {buttonText}
                 </button>
             </div>
-        </div>
     )
 }
 
