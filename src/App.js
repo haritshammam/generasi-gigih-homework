8b4463ade63c7baff88f8129891feacc86b67897
haritshammam <siegkounat@gmail.com>
2021-07-21 02:07:48 +0800

    able to authenticate spotify account, able to search track data using spotify search API, able to show result from GET request from search

--- src/App.js
+++ src/App.js
@@ -1,66 +1,79 @@
-import React from 'react'
-import Card from './components/cards'
-import datas from './datas/tracksData' 
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID
+const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize"
+const REDIRECT_URL_AFTER_LOGIN = "http:%2F%2Flocalhost:3000"
+const SCOPE = "playlist-modify-private"
+
 
 function App() {
-  const valueAssignment = (dataParam) => {
-    let {
-      name: trackTitle,
-      id
-    } = dataParam
+  const [accessToken, setAccessToken] = useState()
+  const [searchKeyword, setSearchKeyword] = useState()
+  const [tracksData, setTracksData] = useState()
 
-    let {
-      name: albumName,
-      artists: [
-        {
-          name: artistName
-        }
-      ],
-      images: [
-        ,
-        {
-          url: albumImageUrl
-        }
-      ]
-    } = dataParam.album
-    let trackDetails = {trackTitle, albumName, artistName}
+  const getParamsFromUrl = (hash) => {
+    const stringAfterHashtag = hash.substring(1)
+    const paramsInUrl = stringAfterHashtag.split("&")
+    const paramsSplitUp = paramsInUrl.reduce((acc, currentVal) => {
+      const [key, value] = currentVal.split("=")
+      acc[key] = value
+      return acc
+    }, {})
 
-    return {
-      id,
-      albumImageUrl,
-      trackDetails
-    }
+    return paramsSplitUp
   }
 
+  const handleLogin = () => {
+    window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPE}&response_type=token&state=123`
+  }
+
+  const handleSearchPlaylist = async () => {
+    await axios
+      .get("https://api.spotify.com/v1/search", {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        },
+        params: {
+          api_key: process.env.REACT_APP_GIPHY_API_KEY,
+          q: searchKeyword,
+          type: "track",
+          limit: 10
+        },
+      })
+      .then((res) => {
+        setTracksData(res.data.tracks.items)
+        console.log(tracksData)
+      })
+  }
+
+  useEffect(() => {
+    if (window.location.hash) {
+      const { access_token } = getParamsFromUrl(window.location.hash)
+      setAccessToken(access_token)
+    }
+  }, [accessToken])
+
   return (
-    <div className="bg-gray-50">
-      <div className="text-center pt-10">
-        <h1
-          className="text-3xl font-bold text-gray-900 mb-4"
-        >
-          Select music to add to your playlist
-        </h1>
+    <div>
+      <button onClick={handleLogin}>Connect with Spotify</button>
+      <br />
+      <br />
+      <input type="text" placeholder="Seach playlist" onChange={(e) => setSearchKeyword(e.target.value)} />
+      <button onClick={handleSearchPlaylist}>Search</button>
 
-        <h2
-          className="text-md text-gray-400"
-        >
-          Showing {datas.length} result
-        </h2>
-      </div>
-
-      <div className="grid grid-cols-4 mt-8 px-10 gap-y-10 place-items-center">
-        {datas.map((data) => {
-          let { id, albumImageUrl, trackDetails } = valueAssignment(data)
-          return (
-            <Card
-              key = {id}
-              trackDetails={trackDetails}
-              albumImageUrl={albumImageUrl}
-              buttonText="Select"
-            />
-          )
-        })}
-      </div>
+      {/* Buat state ketika tracksData masih kosong */}
+      {tracksData.map((track, index) => {
+        return (
+          <div>
+            <p>{index+1}. Track name: {track.name}</p>
+            <p>Artist name: {track.artists[0].name}</p>
+            <br />
+          </div>
+        )
+      })}
     </div>
   )
 }
---
+++ src/constants/spotifyAuthConst.js
@@ -0,0 +1,4 @@
+const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID
+const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize"
+const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000"
+const SCOPES = "playlist-modify-private"
