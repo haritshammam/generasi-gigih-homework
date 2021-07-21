5b43811585737db9e05470a635762b490563f7ab
haritshammam <siegkounat@gmail.com>
2021-07-21 17:35:34 +0800

    Styling added. Splitting into components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { CLIENT_ID, SPOTIFY_AUTHORIZE_ENDPOINT, REDIRECT_URL_AFTER_LOGIN, SCOPE } from './constants/spotifyAuthConst'
 
-const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID
-const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize"
-const REDIRECT_URL_AFTER_LOGIN = "http:%2F%2Flocalhost:3000"
-const SCOPE = "playlist-modify-private"
-
+import styles from './App.module.css'
+import Button from './components/UI/buttons'
+import InputLarge from './components/UI/input-field-large'
+import TrackCard from './components/UI/cards'
+import IconButton from './components/UI/icon-buttons'
 
 function App() {
   const [accessToken, setAccessToken] = useState()
@@ -13,6 +15,7 @@ function App() {
   const [tracksData, setTracksData] = useState()
   const [isAuthenticated, setAuthenticated] = useState(false)
 
+  // To get URL hash that contains tokens info
   const getParamsFromUrl = (hash) => {
     const stringAfterHashtag = hash.substring(1)
     const paramsInUrl = stringAfterHashtag.split("&")
@@ -25,10 +28,12 @@ function App() {
     return paramsSplitUp
   }
 
+  // To go to spotify authentication page
   const handleLogin = () => {
     window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPE}&response_type=token&state=123`
   }
 
+  // To call Spotify Search API
   const handleSearchPlaylist = async () => {
     await axios
       .get("https://api.spotify.com/v1/search", {
@@ -41,12 +46,87 @@ function App() {
           api_key: process.env.REACT_APP_GIPHY_API_KEY,
           q: searchKeyword,
           type: "track",
-          limit: 10
+          limit: 12
         },
       })
       .then((res) => {
         setTracksData(res.data.tracks.items)
       })
+  }
+
+  // To replace space with + in search query
+  const handleKeywordSpace = (keyword) => {
+    const keywordString = keyword.replace(" ", "+")
+    return keywordString
+  }
+
+  // To set search keyword from search input text
+  const handleSetSearchKeyword = (e) => {
+    setSearchKeyword(handleKeywordSpace(e.target.value))
+  }
+
+  // To trigger call handleSearchPlaylist using enter from search input text
+  const handleEnterSearchPlaylist = (e) => {
+    if (e.keyCode === 13) {
+      handleSearchPlaylist()
+    }
+  }
+
+  // To show search tracks components
+  const renderShowTracksPage = () => {
+    if (isAuthenticated) {
+      let renderShowPage = (
+        <div>
+          <div className={styles.search_container}>
+            <InputLarge
+              onChange={handleSetSearchKeyword}
+              onKeyDown={handleEnterSearchPlaylist}
+              placeholder="Search tracks"
+            />
+
+            <div className={styles.search_button}>
+              <IconButton onClick={handleSearchPlaylist}><i class="fas fa-search"></i></IconButton>
+            </div>
+          </div>
+
+          {tracksData && <p className={styles.text_small}>Showing {tracksData.length} result</p>}
+
+          <div className={styles.track_card_list_container}>
+            {tracksData && tracksData.map((track, index) => {
+              return (
+                <TrackCard
+                  key={track.id}
+                  albumImageUrl={track.album.images[1].url}
+                  trackName={track.name}
+                  artistName={track.artists[0].name}
+                />
+              )
+            })}
+          </div>
+        </div>
+      )
+      return renderShowPage
+    }
+  }
+
+  // To show authenticate button if not authenticated yet
+  const renderAuthenticateButton = () => {
+    if (isAuthenticated) {
+      return (
+        <div className={styles.authentication_container}>
+          <h2 className={styles.authentication_heading}>You are authenticated</h2>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div className={styles.authentication_container}>
+          <h2 className={styles.authentication_heading}>You are not authenticated yet</h2>
+          <Button onClick={handleLogin}>Authenticate Spotify</Button>
+        </div>
+      )
+    }
+
   }
 
   useEffect(() => {
@@ -59,46 +139,10 @@ function App() {
     }
   }, [accessToken])
 
-  const handleKeywordSpace = (keyword) => {
-    const keywordString = keyword.replace(" ", "+")
-    return keywordString
-  }
-
-  const renderShowPage = () => {
-    if (isAuthenticated) {
-      let renderShowPage = (
-        <div>
-          <input type="text" placeholder="Seach playlist" onChange={(e) => setSearchKeyword(handleKeywordSpace(e.target.value))} />
-          {console.log(searchKeyword)}
-          <button onClick={handleSearchPlaylist}>Search</button>
-
-          {/* Buat state ketika tracksData masih kosong */}
-          {tracksData && tracksData.map((track, index) => {
-            return (
-              <div>
-                <img src={track.album.images[1].url} alt="Album" />
-                <p>{index + 1}. Track name: {track.name}</p>
-                <p>Artist name: {track.artists[0].name}</p>
-                <br />
-              </div>
-            )
-          })}
-        </div>
-      )
-      return renderShowPage
-    }
-  }
-
-  const renderAuthenticateButton = () => {
-    return (
-      isAuthenticated ? <h2>You are authenticated</h2> : <button onClick={handleLogin}>Connect with Spotify</button>
-    )
-  }
-
   return (
-    <div>
+    <div className={styles.app_container}>
       {renderAuthenticateButton()}
-      {renderShowPage()}
+      {renderShowTracksPage()}
     </div>
   )
 }
--- src/constants/spotifyAuthConst.js
+++ src/constants/spotifyAuthConst.js
@@ -1,4 +1,11 @@
 const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize"
 const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000"
-const SCOPES = "playlist-modify-private"
+const SCOPE = "playlist-modify-private"
+
+export {
+    CLIENT_ID,
+    SPOTIFY_AUTHORIZE_ENDPOINT,
+    REDIRECT_URL_AFTER_LOGIN,
+    SCOPE
+}
